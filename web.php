8b2b14447013f6688add2b57af9b24cd53206df6
Md Mehedi Hassan <96591957+Mehedi5271@users.noreply.github.com>
2023-11-27 02:21:03 +0600

    Add files via upload

--- web.php
+++ web.php
@@ -1,9 +1,18 @@
 <?php
 
+use App\Http\Controllers\BloodGroupController;
+use App\Http\Controllers\DistrictController;
+use App\Http\Controllers\DivisonController;
 use App\Http\Controllers\DonorController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PostOfficeController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UpazillaController;
+use App\Http\Controllers\UserController;
+use App\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Support\Facades\Route;
+use Laravel\Sanctum\Http\Controllers\CsrfCookieController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -24,9 +33,42 @@ Auth::routes();
 
 Route::get('/home', [HomeController::class, 'index'])->name('home');
 
+Route::get('/user',[UserController::class, 'index'])->name('user.index'); //all donor for admin
 
 Route::get('/donor',[DonorController::class, 'donor'])->name('donor.index'); //all donor for admin
 Route::get('/profile/{id}',[DonorController::class, 'profile'])->name('profile.index'); //individual profile for user
+
+Route::get('/blood',[BloodGroupController::class, 'index'])->name('blood.index'); //all donor for admin
+Route::get('/create-blood',[BloodGroupController::class, 'create'])->name('blood.create'); //create division for admin
+Route::post('/blood', [BloodGroupController::class, 'store'])->name('blood.store');
+Route::delete('/blood/{id}', [BloodGroupController::class, 'destroy'])->name('blood.delete');
+
+Route::get('/division', [DivisonController::class, 'divison'])->name('divison.index');
+Route::get('/create-divison',[DivisonController::class, 'create'])->name('divison.create'); //create division for admin
+Route::post('/divison', [DivisonController::class, 'store'])->name('divison.store');
+Route::delete('/divison/{id}', [DivisonController::class, 'destroy'])->name('divison.delete');
+
+Route::get('/district',[DistrictController::class, 'district'])->name('district.index'); //all district for admin 
+Route::get('/create-district',[DistrictController::class, 'create'])->name('district.create'); //create district for admin
+Route::post('/district', [DistrictController::class, 'store'])->name('district.store');
+Route::delete('/district/{id}', [DistrictController::class, 'destroy'])->name('district.delete');
+
+Route::get('/upazilla',[UpazillaController::class, 'upazilla'])->name('upazilla.index'); //all district for admin 
+Route::get('/create-upazilla',[UpazillaController::class, 'create'])->name('upazilla.create'); //create district for admin
+Route::post('/upazilla', [UpazillaController::class, 'store'])->name('upazilla.store');
+Route::delete('/upazilla/{id}', [UpazillaController::class, 'destroy'])->name('upazilla.delete');
+
+Route::get('/post_office',[PostOfficeController::class, 'post_office'])->name('post_office.index'); //all district for admin 
+Route::get('/create-post_office',[PostOfficeController::class, 'create'])->name('post_office.create'); //create district for admin
+Route::post('/post_office', [PostOfficeController::class, 'store'])->name('post_office.store');
+Route::delete('/post_office/{id}', [PostOfficeController::class, 'destroy'])->name('post_office.delete');
+
+Route::get('/profile',[ProfileController::class, 'profile'])->name('profile.index'); //all district for admin 
+Route::get('/create-profile',[ProfileController::class, 'create'])->name('profile.create');
+
+Route::post('/profile', [ProfileController::class, 'store'])->name('profile.store');
+Route::delete('/profile/{id}', [ProfileController::class, 'destroy'])->name('profile.delete');
+
 
 
 
