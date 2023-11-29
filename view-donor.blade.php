a1ee6af6eee74791809a8bda3058370c84960cfb
Md Mehedi Hassan <96591957+Mehedi5271@users.noreply.github.com>
2023-11-29 23:00:26 +0600

    Add files via upload

--- view-donor.blade.php
+++ view-donor.blade.php
@@ -21,232 +21,49 @@
                                 <thead>
                                     <tr>
                                         <th class="serial">#</th>
-                                        <th class="avatar">Profile</th>
+                                        <th class="avatar">Image</th>
                                         <th>Name</th>
+                                        <th>Gender</th>
+                                        <th>Age</th>
+                                        <th>Date of Birth</th>
                                         <th>Blood</th>
                                         <th>Phone</th>
-                                        <th>Address</th>
+                                        <th>Village</th>
                                         <th>District</th>
                                         <th>Last Donate Date</th>
                                         <th>Action</th>
                                     </tr>
                                 </thead>
                                 <tbody>
+                                    @php
+                                        $serial=1;
+                                    @endphp
+
+                                    @foreach ($profiles as $profile)
+                                        
+                                   
                                     <tr>
                                         <td class="serial">1</td>
                                         <td class="avatar">
                                             <div class="round-img">
-                                                <img class="rounded-circle" src="../../images/avatar/1.jpg" alt="admin profile" width="50px" height="50px">
+                                                <img class="rounded-circle" src="" width="50px" height="50px">
                                             </div>
                                         </td>
-                                        <td>Masud Rana</td>
-                                        <td>O+</td>
-                                        <td>01245142587</span></td>
-                                        <td>Belabo</td>
-                                        <td>Narsingdi</td>
-                                        <td>12/08/2020</span></td>
+                                        <td>{{$profile->user->name}}</td>
+                                        <td>{{$profile->gender}}</</td>
+                                        <td>{{$profile->age}}</span></td>
+                                        <td>{{$profile->dob}}</</td>
+                                        <td>{{$profile->bloodGroup->group}}</</span></td>
+                                        <td>{{$profile->user->mobile}}</span></td>
+                                        <td>{{$profile->village}}</span></td>
+                                        <td>{{$profile->district->name}}</span></td>
+                                        <td>{{$profile->last_donate}}</span></td>
                                         <td>
-                                            <a href="donor-details.html" class="btn btn-success btn-sm">View</a>
+                                            <a href="donor-details.html" class="btn btn-danger btn-sm">Delete</a>
                                         </td>
                                     </tr>
-
-                                    <tr>
-                                        <td class="serial">2</td>
-                                        <td class="avatar">
-                                            <div class="round-img">
-                                                <img class="rounded-circle" src="../../images/avatar/2.jpg" alt="admin profile" width="50px" height="50px">
-                                            </div>
-                                        </td>
-                                        <td>Lorum lorum</td>
-                                        <td>O+</td>
-                                        <td>01242452587</span></td>
-                                        <td>Nondigram</td>
-                                        <td>Bogura</td>
-                                        <td>17/09/2019</span></td>
-                                        <td>
-                                            <a href="donor-details.html" class="btn btn-success btn-sm">View</a>
-                                        </td>
-                                    </tr>
-
-                                    <tr>
-                                        <td class="serial">3</td>
-                                        <td class="avatar">
-                                            <div class="round-img">
-                                                <img class="rounded-circle" src="../../images/avatar/3.jpg" alt="admin profile" width="50px" height="50px">
-                                            </div>
-                                        </td>
-                                        <td>Rakib Hasaa</td>
-                                        <td>AB+</td>
-                                        <td>01245528587</span></td>
-                                        <td>Dhunot</td>
-                                        <td>Bogura</td>
-                                        <td>12/12/2020</span></td>
-                                        <td>
-                                            <a href="donor-details.html" class="btn btn-success btn-sm">View</a>
-                                        </td>
-                                    </tr>
-
-                                    <tr>
-                                        <td class="serial">4</td>
-                                        <td class="avatar">
-                                            <div class="round-img">
-                                                <img class="rounded-circle" src="../../images/avatar/4.jpg" alt="admin profile" width="50px" height="50px">
-                                            </div>
-                                        </td>
-                                        <td>Ebrahim Kardi</td>
-                                        <td>B-</td>
-                                        <td>0124514524</span></td>
-                                        <td>Madobpur</td>
-                                        <td>Habigonj</td>
-                                        <td>12/01/2021</span></td>
-                                        <td>
-                                            <a href="donor-details.html" class="btn btn-success btn-sm">View</a>
-                                        </td>
-                                    </tr>
-
-                                    <tr>
-                                        <td class="serial">5</td>
-                                        <td class="avatar">
-                                            <div class="round-img">
-                                                <img class="rounded-circle" src="../../images/avatar/5.jpg" alt="admin profile" width="50px" height="50px">
-                                            </div>
-                                        </td>
-                                        <td>Milon Mia</td>
-                                        <td>O+</td>
-                                        <td>01245642587</span></td>
-                                        <td>Belabo</td>
-                                        <td>Narsingdi</td>
-                                        <td>12/08/2020</span></td>
-                                        <td>
-                                            <a href="donor-details.html" class="btn btn-success btn-sm">View</a>
-                                        </td>
-                                    </tr>
-
-                                    <tr>
-                                        <td class="serial">6</td>
-                                        <td class="avatar">
-                                            <div class="round-img">
-                                                <img class="rounded-circle" src="../../images/avatar/1.jpg" alt="admin profile" width="50px" height="50px">
-                                            </div>
-                                        </td>
-                                        <td>Mehedi Hasan</td>
-                                        <td>A+</td>
-                                        <td>0124542587</span></td>
-                                        <td>Bancarampur</td>
-                                        <td>Brahmanbaria</td>
-                                        <td>12/08/2020</span></td>
-                                        <td>
-                                            <a href="donor-details.html" class="btn btn-success btn-sm">View</a>
-                                        </td>
-                                    </tr>
-                                    
-                                    <tr>
-                                        <td class="serial">7</td>
-                                        <td class="avatar">
-                                            <div class="round-img">
-                                                <img class="rounded-circle" src="../../images/avatar/1.jpg" alt="admin profile" width="50px" height="50px">
-                                            </div>
-                                        </td>
-                                        <td>Masud Rana</td>
-                                        <td>O+</td>
-                                        <td>01245142587</span></td>
-                                        <td>Belabo</td>
-                                        <td>Narsingdi</td>
-                                        <td>12/08/2020</span></td>
-                                        <td>
-                                            <a href="donor-details.html" class="btn btn-success btn-sm">View</a>
-                                        </td>
-                                    </tr>
-
-                                    <tr>
-                                        <td class="serial">8</td>
-                                        <td class="avatar">
-                                            <div class="round-img">
-                                                <img class="rounded-circle" src="../../images/avatar/2.jpg" alt="admin profile" width="50px" height="50px">
-                                            </div>
-                                        </td>
-                                        <td>Lorum lorum</td>
-                                        <td>O+</td>
-                                        <td>01242452587</span></td>
-                                        <td>Nondigram</td>
-                                        <td>Bogura</td>
-                                        <td>17/09/2019</span></td>
-                                        <td>
-                                            <a href="donor-details.html" class="btn btn-success btn-sm">View</a>
-                                        </td>
-                                    </tr>
-
-                                    <tr>
-                                        <td class="serial">9</td>
-                                        <td class="avatar">
-                                            <div class="round-img">
-                                                <img class="rounded-circle" src="../../images/avatar/3.jpg" alt="admin profile" width="50px" height="50px">
-                                            </div>
-                                        </td>
-                                        <td>Rakib Hasaa</td>
-                                        <td>AB+</td>
-                                        <td>01245528587</span></td>
-                                        <td>Dhunot</td>
-                                        <td>Bogura</td>
-                                        <td>12/12/2020</span></td>
-                                        <td>
-                                            <a href="donor-details.html" class="btn btn-success btn-sm">View</a>
-                                        </td>
-                                    </tr>
-
-                                    <tr>
-                                        <td class="serial">10</td>
-                                        <td class="avatar">
-                                            <div class="round-img">
-                                                <img class="rounded-circle" src="../../images/avatar/4.jpg" alt="admin profile" width="50px" height="50px">
-                                            </div>
-                                        </td>
-                                        <td>Ebrahim Kardi</td>
-                                        <td>B-</td>
-                                        <td>0124514524</span></td>
-                                        <td>Madobpur</td>
-                                        <td>Habigonj</td>
-                                        <td>12/01/2021</span></td>
-                                        <td>
-                                            <a href="donor-details.html" class="btn btn-success btn-sm">View</a>
-                                        </td>
-                                    </tr>
-
-                                    <tr>
-                                        <td class="serial">11</td>
-                                        <td class="avatar">
-                                            <div class="round-img">
-                                                <img class="rounded-circle" src="../../images/avatar/5.jpg" alt="admin profile" width="50px" height="50px">
-                                            </div>
-                                        </td>
-                                        <td>Milon Mia</td>
-                                        <td>O+</td>
-                                        <td>01245642587</span></td>
-                                        <td>Belabo</td>
-                                        <td>Narsingdi</td>
-                                        <td>12/08/2020</span></td>
-                                        <td>
-                                            <a href="donor-details.html" class="btn btn-success btn-sm">View</a>
-                                        </td>
-                                    </tr>
-
-                                    <tr>
-                                        <td class="serial">12</td>
-                                        <td class="avatar">
-                                            <div class="round-img">
-                                                <img class="rounded-circle" src="../../images/avatar/1.jpg" alt="admin profile" width="50px" height="50px">
-                                            </div>
-                                        </td>
-                                        <td>Mehedi Hasan</td>
-                                        <td>A+</td>
-                                        <td>0124542587</span></td>
-                                        <td>Bancarampur</td>
-                                        <td>Brahmanbaria</td>
-                                        <td>12/08/2020</span></td>
-                                        <td>
-                                            <a href="donor-details.html" class="btn btn-success btn-sm">View</a>
-                                        </td>
-                                    </tr>
+                                    @endforeach
+                         
 
                                 </tbody>
                             </table>
